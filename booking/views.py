588e0e89016e06190273ba28b09324c3cf232d91
Gregory <stary.grzegorz@gmail.com>
2025-05-08 11:28:11 +0000

    ADD: Implement homepage, about, and contact views with room availability logic

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Hotels,Rooms,Reservation
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the booking index.")
+
+# Homepage
+
+def homepage(request):
+    all_location = Hotels.objects.values_list('location','id').distinct().order_by()
+    if request.method =="POST":
+        try:
+            print(request.POST)
+            hotel = Hotels.objects.all().get(id=int(request.POST['search_location']))
+            rr = []
+            
+            #for finding the reserved rooms on this time period for excluding from the query set
+            for each_reservation in Reservation.objects.all():
+                if str(each_reservation.check_in) < str(request.POST['cin']) and str(each_reservation.check_out) < str(request.POST['cout']):
+                    pass
+                elif str(each_reservation.check_in) > str(request.POST['cin']) and str(each_reservation.check_out) > str(request.POST['cout']):
+                    pass
+                else:
+                    rr.append(each_reservation.room.id)
+                
+            room = Rooms.objects.all().filter(hotel=hotel,capacity__gte = int(request.POST['capacity'])).exclude(id__in=rr)
+            if len(room) == 0:
+                messages.warning(request,"Sorry No Rooms Are Available on this time period")
+            data = {'rooms':room,'all_location':all_location,'flag':True}
+            response = render(request,'index.html',data)
+        except Exception as e:
+            messages.error(request,e)
+            response = render(request,'index.html',{'all_location':all_location})
+
+
+    else:
+        
+        
+        data = {'all_location':all_location}
+        response = render(request,'index.html',data)
+    return HttpResponse(response)
+
+# About
+def aboutpage(request):
+    return HttpResponse(render(request,'about.html'))
+
+# Contact page
+def contactpage(request):
+    return HttpResponse(render(request,'contact.html'))
