ef2f872e61bbcbf2ae5130fb6c9d091889139797
Gregory <stary.grzegorz@gmail.com>
2025-05-08 10:54:55 +0000

    ADD: Define Hotels models

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Hotels(models.Model):
+    #h_id,h_name,owner ,location,rooms
+    name = models.CharField(max_length=30,default="Stary-Condohotel")
+    owner = models.CharField(max_length=20)
+    location = models.CharField(max_length=50)
+    state = models.CharField(max_length=50,default="Palawan")
+    country = models.CharField(max_length=50,default="Philippines")
+    def __str__(self):
+        return self.name
