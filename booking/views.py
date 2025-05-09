06bd69071962f50bedc5565b9e4d981237d8f0b3
Gregory <stary.grzegorz@gmail.com>
2025-05-09 02:45:23 +0000

    ADD: Implement user and staff sign-up functionality with validation.

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Hotels,Rooms,Reservation
 from django.contrib import messages
@@ -49,6 +49,61 @@ def homepage(request):
 def aboutpage(request):
     return HttpResponse(render(request,'about.html'))
 
-# Contact page
+# Contact Page
 def contactpage(request):
     return HttpResponse(render(request,'contact.html'))
+
+# User Sign-Up
+def user_sign_up(request):
+    if request.method =="POST":
+        user_name = request.POST['username']
+        
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.warning(request,"Password didn't matched")
+            return redirect('userloginpage')
+        
+        try:
+            if User.objects.all().get(username=user_name):
+                messages.warning(request,"Username Not Available")
+                return redirect('userloginpage')
+        except:
+            pass
+            
+
+        new_user = User.objects.create_user(username=user_name,password=password1)
+        new_user.is_superuser=False
+        new_user.is_staff=False
+        new_user.save()
+        messages.success(request,"Registration Successfull")
+        return redirect("userloginpage")
+    return HttpResponse('Access Denied')
+#staff sign up
+def staff_sign_up(request):
+    if request.method =="POST":
+        user_name = request.POST['username']
+        
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.success(request,"Password didn't Matched")
+            return redirect('staffloginpage')
+        try:
+            if User.objects.all().get(username=user_name):
+                messages.warning(request,"Username Already Exist")
+                return redirect("staffloginpage")
+        except:
+            pass
+        
+        new_user = User.objects.create_user(username=user_name,password=password1)
+        new_user.is_superuser=False
+        new_user.is_staff=True
+        new_user.save()
+        messages.success(request," Staff Registration Successfull")
+        return redirect("staffloginpage")
+    else:
+
+        return HttpResponse('Access Denied')
